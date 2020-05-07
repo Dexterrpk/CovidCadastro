8e2a40b2276439999e1003dc26df476766860539
cleiton <cleitonneri04@hotmail.com>
2020-05-07 20:09:49 -0300

    3

--- app/src/main/java/com/dexter/covid/view/CadastroAdapter.java
+++ app/src/main/java/com/dexter/covid/view/CadastroAdapter.java
@@ -12,7 +12,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-
 import com.dexter.covid.R;
 import com.dexter.covid.database.model.Note;
 
